e15f9925cd60b774fa19e1f6c096bb616900cfd9
Chinmay Hardikar <hardikarchinmay1993@gmail.com>
2018-02-17 10:16:48 +0530

    Update http.service.ts
    Removed Flickr API key

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -6,7 +6,7 @@ export class HttpService {
   constructor(private http: Http) { }
 
   getPaginatorData(pageNumber) {
-    return this.http.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=kitten&api_key=c124f76ad3ea206cd2c440bf2d5f0e98&format=json&nojsoncallback=?&per_page=30&page=${pageNumber}`)
+    return this.http.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=kitten&api_key={flickr_api_key}&format=json&nojsoncallback=?&per_page=30&page=${pageNumber}`)
   }
 
 }
